de164fc1e8a548117f9176659ae8092d4bd242c0
Soma Makai <soma.makai@gmail.com>
2020-10-29 16:34:56 +0100

    accountadmin started

--- GPFrontend/src/app/components/+dashboard/dashboard-accountadmin/dashboard-accountadmin.component.ts
+++ GPFrontend/src/app/components/+dashboard/dashboard-accountadmin/dashboard-accountadmin.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ListPagination } from 'src/app/core/pagination/list-pagination';
+import { AccountService } from 'src/app/services/account/account.service';
+import { EntityListResult, Sorting } from 'src/app/services/common/entity.service';
+import { Account } from 'src/app/services/account/account';
 
 @Component({
   selector: 'dashboard-accountadmin',
@@ -7,9 +11,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardAccountadminComponent implements OnInit {
 
-  constructor() { }
+  accountListResult = new EntityListResult<Account>();
+
+  accounts: Account[] = [];
+  selectedAccount: string = "";
+
+  constructor(private accountService: AccountService) { }
 
   ngOnInit() {
+    this.getAccountList();
   }
 
+  getAccountList(pagination: ListPagination = null, sorting: Sorting = null, filter: string = null) {
+    this.accountListResult.onFinished = () => {
+  }
+    
+    this.accountService.getList(this.accountListResult, pagination || new ListPagination(), sorting || new Sorting(), filter || "");
+  }
 }
